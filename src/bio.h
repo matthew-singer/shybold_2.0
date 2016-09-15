d86798c908ad25c67ac371cf27d26be69672eaa9
David Streett <davidastreett@hotmail.com>
2016-09-15 09:50:27 -0700

    Updated bio

--- src/bio.h
+++ src/bio.h
@@ -2,11 +2,14 @@
 #define BIO_H_
 
 #include <vector>
+#include <memory>
+
+
 #include "params.h"
 
 
 class chrome {
-
+public:
 	chrome() {
 		for (vector<double>iterator dbl_it = values.begin(); dbl_it != values.end(); ++dbl_it) {
 			if (mutate) {
@@ -15,8 +18,20 @@ class chrome {
 		}
 
 	}	
+
 	std::vector<double> values(geneCount);
 
+
+}
+
+class genome {
+public:
+	std::unique_ptr<chrome> c1;
+	std::unique_ptr<chrome> c1;
+	
+	class genome(c *c1, chrome *c2) {
+		
+	}
 
 }
 
